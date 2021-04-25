b9690dc29ab642a0843565a824333720544d827b
ellatom <ella271@gmail.com>
2021-04-25 20:00:23 +0300

    fixed bug

--- Util/dynamicClass.js
+++ Util/dynamicClass.js
@@ -2,7 +2,7 @@ import Elephant from '../Animals/elephant.js';
 import Rabbit from '../Animals/rabbit.js';
 import Penguin from '../Animals/penguin.js';
 //add here new classes
-const classes = { Elephant, Rabbit,Penguin };
+const classes = { Elephant,Rabbit,Penguin };
 //Factory function that performs a lookup against an object of class name.
 //class mappings and returns reference to the class
 export default function dynamicClass (name) {
